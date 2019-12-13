0965a5a1597ab973e7a47aefa0c1da2afd070d84
josanosouza <josano.soares@scnbrasil.com.br>
2019-12-13 17:33:40 -0300

    Adicionando autenticação jwt

--- routes/api.php
+++ routes/api.php
@@ -5,7 +5,10 @@ $this->post('categories', 'Api\CategoryController@store');
 $this->put('categories/{id}', 'Api\CategoryController@update');
 $this->delete('categories/{id}', 'Api\CategoryController@delete');*/
 
+$this->post('auth', 'Auth\AuthApiController@authenticate');
+
 $this->group(['prefix' => 'v1', 'namespace' => 'Api\v1'], function () {
+    
     $this->get('categories/{id}/products', 'CategoryController@products');
     $this->apiresource('categories', 'CategoryController');
     
